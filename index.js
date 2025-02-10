8ce5a320049ca5922f3db81b21e0088918980de8
JustinHodge <51083905+JustinHodge@users.noreply.github.com>
2025-02-10 05:12:18 +0000

    Build: ()

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ const server = app.listen(port, () => {
 const wss = new ws_1.WebSocketServer({ server: server });
 const scorePads = new scorePads_1.default();
 app.use((0, cors_1.default)());
-app.use(express_1.default.static(path_1.default.resolve('../public')));
+app.use(express_1.default.static(path_1.default.resolve('./public')));
 app.get('/', (req, res) => {
     process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'
-        ? res.sendFile(path_1.default.resolve('../public/client/index.html'))
+        ? res.sendFile(path_1.default.resolve('./public/client/index.html'))
         : res.send('Development Server. Use Vite Frontend.');
 });
 app.get('/scorepads', (req, res) => {
