621fb056874701cb4d6abab7576552fa2a2abd2b
JustinHodge <justinhodge8@gmail.com>
2025-02-24 21:28:21 -0500

    style new score pad

--- client/src/components/new_score_pad/components/join_existing/join_existing.tsx
+++ client/src/components/new_score_pad/components/join_existing/join_existing.tsx
@@ -6,20 +6,28 @@ export const JoinExisting = () => {
     const { requestJoinExisting } = useContext(ScorepadContext);
     const [scorePadIdToJoin, setScorePadIdToJoin] = useState<string>('');
     return (
-        <div>
-            <label>Score Pad ID</label>
-            <input
-                type='string'
-                onChange={(e) => setScorePadIdToJoin(e.target.value)}
-            ></input>
-            <button
-                type='button'
-                onClick={() => {
-                    requestJoinExisting(scorePadIdToJoin);
-                }}
-            >
-                Join Open Score Pad
-            </button>
+        <div className='row'>
+            <form>
+                <div className='pt-2'>
+                    <label className='form-label'>Score Pad ID</label>
+                    <input
+                        className='form-control'
+                        type='string'
+                        onChange={(e) => setScorePadIdToJoin(e.target.value)}
+                    ></input>
+                </div>
+                <div className='float-end pt-2'>
+                    <button
+                        className='btn btn-primary'
+                        type='button'
+                        onClick={() => {
+                            requestJoinExisting(scorePadIdToJoin);
+                        }}
+                    >
+                        Join Open Score Pad
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
