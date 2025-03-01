8ddfb4841f8de4de72b210684f3e78fdc7728bfd
JustinHodge <justinhodge8@gmail.com>
2025-03-01 00:04:55 -0500

    more styling

---
+++ client/src/components/score_pad/components/scorepad_navbar/scorepad_navbar.tsx
@@ -0,0 +1,56 @@
+import { Navbar, Image, NavbarText, Button } from 'react-bootstrap';
+import './scorepad_navbar.css';
+import { useContext } from 'react';
+import ScorepadContext from '../../../../contexts/score_pad';
+
+export const ScorepadNavbar = () => {
+    const { requestAddPlayer, requestLeaveExisting } =
+        useContext(ScorepadContext);
+    return (
+        <Navbar sticky='top' bg='dark' className='px-2 shadow-lg'>
+            <Navbar.Brand>
+                <Image
+                    title='Click to copy shareable url'
+                    className='header-image'
+                    onPointerDown={() => {
+                        const url = new URL(window.location.href);
+                        navigator.clipboard.writeText(url.toString());
+                        alert('Copied Url: ' + url.toString());
+                    }}
+                    src='/scorepad_logo.svg'
+                    alt='logo'
+                />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse className='justify-content-end'>
+                <NavbarText className='px-2'>
+                    <Button
+                        onClick={() => {
+                            requestAddPlayer(0);
+                        }}
+                    >
+                        <Image
+                            className='header-image-small'
+                            src='/add_profile.svg'
+                        />
+                    </Button>
+                </NavbarText>
+                <NavbarText className='px-2'>
+                    <Button
+                        variant='danger'
+                        onClick={() => {
+                            requestLeaveExisting();
+                        }}
+                    >
+                        <Image
+                            className='header-image-small'
+                            src='/session_leave.svg'
+                        />
+                    </Button>
+                </NavbarText>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default ScorepadNavbar;
--- client/src/components/score_pad/score_pad.tsx
+++ client/src/components/score_pad/score_pad.tsx
@@ -1,16 +1,18 @@
 import { Container } from 'react-bootstrap';
 import PlayerFormList from './components/player_form_list/player_form_list';
-import ScorepadHeading from './components/scorepad_heading/scorepad_heading';
+import ScorepadNavbar from './components/scorepad_navbar/scorepad_navbar';
 
 export const Scorepad = () => {
     return (
-        <Container>
-            <ScorepadHeading />
-            <div className='container'>
-                <div className='row'>
-                    <PlayerFormList />
+        <>
+            <ScorepadNavbar />
+            <Container>
+                <div className='container'>
+                    <div className='row'>
+                        <PlayerFormList />
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </>
     );
 };
