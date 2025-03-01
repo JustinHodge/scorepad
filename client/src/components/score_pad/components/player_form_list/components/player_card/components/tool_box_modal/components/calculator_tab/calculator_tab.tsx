7e7767c91b13450b89285f610b502f54d02cda9b
JustinHodge <justinhodge8@gmail.com>
2025-03-01 14:06:45 -0500

    calculator initial style

--- client/src/components/score_pad/components/player_form_list/components/player_card/components/tool_box_modal/components/calculator_tab/calculator_tab.tsx
+++ client/src/components/score_pad/components/player_form_list/components/player_card/components/tool_box_modal/components/calculator_tab/calculator_tab.tsx
@@ -1,3 +1,51 @@
+import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+
+const CALCULATOR_BUTTONS = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    0,
+    '.',
+    '=',
+    '+',
+    '-',
+    '*',
+    '/',
+];
+
+const BUTTONS_PER_ROW = 4;
 export const CalculatorTab = () => {
-    return <div> Calculator Tab </div>;
+    return (
+        <Card border='light' className='p-4 m-4 shadow-lg'>
+            <Container>
+                <Row className='mb-4'>
+                    <Form.Control
+                        className='bg-light text-end text-dark'
+                        type='text'
+                    />
+                </Row>
+                <Row xs={BUTTONS_PER_ROW} className='g-2'>
+                    {CALCULATOR_BUTTONS.map((button) => {
+                        return (
+                            <Col className='mb-1' key={button}>
+                                <Button
+                                    variant='secondary'
+                                    style={{ width: '100%' }}
+                                    type='button'
+                                >
+                                    {button}
+                                </Button>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </Card>
+    );
 };
