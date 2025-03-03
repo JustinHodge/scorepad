c7e6b052c3b299ab13ac31d42db116da50e3d3a3
JustinHodge <justinhodge8@gmail.com>
2025-03-02 22:56:00 -0500

    initial d4 built

--- client/src/components/score_pad/components/player_form_list/components/player_card/components/tool_box_modal/components/dice_tab/dice_tab.tsx
+++ client/src/components/score_pad/components/player_form_list/components/player_card/components/tool_box_modal/components/dice_tab/dice_tab.tsx
@@ -2,6 +2,7 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import './dice_tab.css';
 import Coin from './coin/coin';
 import D6 from './d6/d6';
+import D4 from './d4/d4';
 
 export const DiceTab = () => {
     return (
@@ -14,6 +15,12 @@ export const DiceTab = () => {
                     <Col xs={6}>
                         <D6 />
                     </Col>
+                </Row>
+                <Row>
+                    <Col xs={6}>
+                        <D4 />
+                    </Col>
+                    <Col xs={6}></Col>
                 </Row>
             </Container>
         </Card>
