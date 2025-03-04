7f10adb85860a119af0c3cc20667dd9f6b0a01b2
JustinHodge <justinhodge8@gmail.com>
2025-03-03 21:42:34 -0500

    remove d4

--- client/src/components/score_pad/components/player_form_list/components/player_card/components/tool_box_modal/components/dice_tab/dice_tab.tsx
+++ client/src/components/score_pad/components/player_form_list/components/player_card/components/tool_box_modal/components/dice_tab/dice_tab.tsx
@@ -2,7 +2,6 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import './dice_tab.css';
 import Coin from './coin/coin';
 import D6 from './d6/d6';
-import D4 from './d4/d4';
 
 export const DiceTab = () => {
     return (
@@ -15,12 +14,6 @@ export const DiceTab = () => {
                     <Col xs={6}>
                         <D6 />
                     </Col>
-                </Row>
-                <Row>
-                    <Col xs={6}>
-                        <D4 />
-                    </Col>
-                    <Col xs={6}></Col>
                 </Row>
             </Container>
         </Card>
